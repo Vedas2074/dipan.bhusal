20939e63481242970a67ce3850dcf7fca87f412c
xxxx <xxxx@xxxxs-MacBook-Air.local>
2020-09-29 10:32:25 +0545

    Added: array in C#

--- CSharpbasics/Array.cs
+++ CSharpbasics/Array.cs
@@ -1,9 +1,27 @@
 class Array
 {
-    void Learn1DArray()
+    void learn1DArray()
     {
         byte[] array1 = new byte[10];
-        array1[0] = 22;
-        float[] array2 = new float[] {2.1f, 4.3f};
+        array1[0] = 23;
+        float[] array2 = new float[] { 2.1f, 34.5f, 62.8f };
+        array2[10] = 28;
+        char[] array3 = { 'A', 'B', 'C', 'D' };
+        float[] subArray1 = array2;
+    }
+    void LearnMulDArray()
+    {
+        int[,] numbers1 = new int[3, 4];
+        numbers1[0, 0] = 34892;
+        decimal[,] numbers2 = { { 2.2m, 3.6m }, { 14.2m, 12.8m } };
+        numbers2[2, 3] = 879;
+        decimal[,] numbers3 = { { 1.2m, 3.6m }, { 22.1m, 1.1m }, { 19.1m, 123.2m } }
+    }
+    void LearnJaggedArray()
+    {
+        short[][] numbers = new short[][];
+        numbers[0] = new short[] { 2, 3, 4 };
+        numbers[1] = new short[] { 3, 4 };
+        numbers[2] = new short[] { 3, 4, 5, 8 };
     }
 }
