7c7ca0f0533987d4b7ebd2c09f4c53000e49615a
xxxx <xxxx@xxxxs-MacBook-Air.local>
2020-10-12 10:16:49 +0545

    Fixed some errors

--- CSharpbasics/Array.cs
+++ CSharpbasics/Array.cs
@@ -15,7 +15,7 @@ class Array
         numbers1[0, 0] = 34892;
         decimal[,] numbers2 = { { 2.2m, 3.6m }, { 14.2m, 12.8m } };
         numbers2[2, 3] = 879;
-        decimal[,] numbers3 = { { 1.2m, 3.6m }, { 22.1m, 1.1m }, { 19.1m, 123.2m } }
+        decimal[,] numbers3 = { { 1.2m, 3.6m }, { 22.1m, 1.1m }, { 19.1m, 123.2m } };
     }
     
 }
